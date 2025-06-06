67e4a7837e23b462204ab45bb179332216e83ef1
Arthur <arthursousarabelo2@gmail.com>
2025-06-06 16:37:41 -0300

    chore: update dependencies and configure NativeWind
    - Added NativeWind and TailwindCSS to the project.
    - Updated react-native-reanimated version to 3.16.2.
    - Configured TypeScript paths to point to the src directory.
    - Created VSCode settings for BiomeJS as the default formatter.
    - Added Babel configuration for NativeWind support.
    - Created Biome configuration for code formatting and linting.
    - Configured Metro bundler to work with NativeWind.
    - Added global CSS file for TailwindCSS styles.
    - Updated TailwindCSS configuration to include component paths.
    - Created a basic Home component to test the setup.

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import '@/styles/global.css'
+
+import { Text, View } from 'react-native'
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello World</Text>
+    </View>
+  )
+}
